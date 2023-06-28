ac46a72dfec7f8d09c6e33946ec3101b5049b40f
Sam Rosenberg <serosenberg99@gmail.com>
2023-06-28 12:33:03 -0500

    Fixing width issues and responsiveness

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,69 @@
-import './style.css'
+import "./style.css";
 
-import {AmbientLight, MathUtils, Mesh, MeshStandardMaterial, PerspectiveCamera, PointLight, Scene, SphereGeometry, TorusGeometry, WebGLRenderer, TextureLoader, MeshBasicMaterial, BoxGeometry} from 'three';
+import {
+    AmbientLight,
+    MathUtils,
+    Mesh,
+    MeshStandardMaterial,
+    PerspectiveCamera,
+    PointLight,
+    Scene,
+    SphereGeometry,
+    TorusGeometry,
+    WebGLRenderer,
+    TextureLoader,
+    MeshBasicMaterial,
+    BoxGeometry,
+} from "three";
 //import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 // Setup
 const scene = new Scene();
-const camera = new PerspectiveCamera(75, window.innerWidth/window.innerHeight, .01, 1000)
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
 const renderer = new WebGLRenderer({
-  canvas: document.querySelector('#app')!
-})
+    canvas: document.querySelector("#app")!,
+});
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 // Shapes
-const geometry = new TorusGeometry(10,3,16,100);
-const material = new MeshStandardMaterial({color: 0x7cfc00});
+const geometry = new TorusGeometry(10, 3, 16, 100);
+const material = new MeshStandardMaterial({ color: 0x7cfc00 });
 const torus = new Mesh(geometry, material);
 scene.add(torus);
 
-const box = new BoxGeometry(3,3,3);
-const samTexture = new TextureLoader().load('portrait.png');
-const sphereMaterial = new MeshBasicMaterial({map: samTexture});
-const sam = new Mesh(box,sphereMaterial);
+var dim = window.innerWidth / 500;
+const box = new BoxGeometry(dim, dim, dim);
+const samTexture = new TextureLoader().load("portrait.png");
+const sphereMaterial = new MeshBasicMaterial({ map: samTexture });
+const sam = new Mesh(box, sphereMaterial);
 scene.add(sam);
-sam.position.z=35;
-sam.position.x=-4;
-sam.position.y=.5;
+sam.position.z = window.innerHeight / 25;
+sam.position.x = -4;
+sam.position.y = 0.5;
 
-const heart = new SphereGeometry(15);
-const pittTexture=new TextureLoader().load('pitt2.png');
-const heartMaterial=new MeshBasicMaterial({map:pittTexture});
-const Pitt = new Mesh(heart,heartMaterial);
+dim = window.innerWidth / 100;
+const heart = new SphereGeometry(dim);
+const pittTexture = new TextureLoader().load("pitt2.png");
+const heartMaterial = new MeshBasicMaterial({ map: pittTexture });
+const Pitt = new Mesh(heart, heartMaterial);
 scene.add(Pitt);
-Pitt.position.z=120;
-Pitt.position.x=-50;
+Pitt.position.z = 120;
+Pitt.position.x = -50;
 
 function addStar() {
-  const geometry = new SphereGeometry(0.25, 24, 24);
-  const material = new MeshStandardMaterial({ color: 0xffffff });
-  const star = new Mesh(geometry, material);
+    const geometry = new SphereGeometry(0.25, 24, 24);
+    const material = new MeshStandardMaterial({ color: 0xffffff });
+    const star = new Mesh(geometry, material);
 
-  const [x, y, z] = Array(3).fill(0).map(() => MathUtils.randFloatSpread(500));
+    const [x, y, z] = Array(3)
+        .fill(0)
+        .map(() => MathUtils.randFloatSpread(500));
 
-  star.position.set(x, y, z);
-  scene.add(star);
+    star.position.set(x, y, z);
+    scene.add(star);
 }
 
 const stars = Array(500).fill(0);
@@ -53,11 +71,10 @@ stars.forEach(addStar);
 
 // Light Sources
 const pointLight = new PointLight(0xffffff);
-pointLight.position.set(0,0,0);
+pointLight.position.set(0, 0, 0);
 
-const ambientLight=new AmbientLight(0xffffff);
-scene.add(pointLight,ambientLight);
-
+const ambientLight = new AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
 
 // Helpers
 //TODO: Remove for prod
@@ -67,30 +84,37 @@ scene.add(pointLight,ambientLight);
 //  scene.add(lightHelper, gridHelper);
 
 function moveCamera() {
-  const t = document.body.getBoundingClientRect().top;
+    const t = document.body.getBoundingClientRect().top;
 
-  camera.position.z = t * -0.05;
-  camera.position.x = t * -0.0002;
-  camera.rotation.y = t * -0.0002;
+    camera.position.z = t * -0.05;
+    camera.position.x = t * -0.0002;
+    camera.rotation.y = t * -0.0002;
 }
 
 document.body.onscroll = moveCamera;
 moveCamera();
 
-function animate(){
-  requestAnimationFrame(animate);
+function animate() {
+    requestAnimationFrame(animate);
 
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
 
-  sam.rotation.x += 0.01;
-  sam.rotation.y += 0.005;
-  sam.rotation.z += 0.01;
+    sam.position.z = 0.03 * window.innerWidth;
+    sam.position.x = -0.003 * window.innerWidth;
+    sam.position.y = 0.001 * window.innerHeight;
 
-  Pitt.rotation.y += 0.05;
+    sam.rotation.x += 0.01;
+    sam.rotation.y += 0.005;
+    sam.rotation.z += 0.01;
 
-  renderer.render(scene, camera);
+    Pitt.position.z = 0.1 * window.innerWidth;
+    Pitt.position.x = -0.05 * window.innerWidth;
+
+    Pitt.rotation.y += 0.05;
+
+    renderer.render(scene, camera);
 }
 
 animate();
